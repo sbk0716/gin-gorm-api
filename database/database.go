a66897e95dddc93abced88bc1b07c8645c84a134
sbk0716 <uhey0716@gmail.com>
2023-05-07 16:54:00 +0900

    update db connect

--- database/database.go
+++ database/database.go
@@ -10,8 +10,11 @@ import (
 
 var Database *gorm.DB
 
-// Connect to the database.
-// FYI: https://gorm.io/docs/connecting_to_the_database.html#PostgreSQL
+/*
+1. Retrieves the environment variables required to set up a database connection.
+2. Opens the connection using the GORM PostgreSQL driver.
+FYI: https://gorm.io/docs/connecting_to_the_database.html#PostgreSQL
+*/
 func Connect() {
 	var err error
 	host := os.Getenv("DB_HOST")
@@ -23,9 +26,11 @@ func Connect() {
 	// Set Data Source Name.
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
 	// Set dialector.
-	dialector := postgres.Open(dsn)
-	// Open initialize db session based on dialector.
-	Database, err = gorm.Open(dialector, &gorm.Config{})
+	pgDialector := postgres.Open(dsn)
+	// Set options.
+	pgOpts := &gorm.Config{}
+	// gorm.Open function opens initialize db session based on dialector.
+	Database, err = gorm.Open(pgDialector, pgOpts)
 
 	if err != nil {
 		// The panic built-in function stops normal execution of the current goroutine.
